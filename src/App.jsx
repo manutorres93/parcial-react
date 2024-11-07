cb9d963c1527466866f4b7b0ad72ab7a523d41ca
manutorres93 <manutorres9312@gmail.com>
2024-11-07 18:36:20 -0500

    Entrega parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import './App.css'
-import Card from './Components/Card'
-import { useState } from "react";
+// App.js
+import './App.css';
+import Card from './Components/Card';
+import { useState } from 'react';
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [submittedName, setSubmittedName] = useState("");
+  const [name, setName] = useState('');
+  const [house, setHouse] = useState('');
+  const [showCard, setShowCard] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setSubmittedName(name);
+    
+    if (name.trim().length >= 3 && !name.startsWith(' ') && house.length >= 6) {
+      setShowCard(true); 
+      setError(false);
+    } else {
+      setError(true);
+      setShowCard(false);
+    }
   };
-
 
   return (
     <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Ingresa tu nombre"
-        />
-        <button type="submit">Enviar</button>
-      </form>
-      {submittedName && <Card message={`Hola, ${submittedName}`} />}
+      <h2>Tu casa de Hogwarts</h2>
+      {showCard ? (
+        <Card name={name} house={house} />
+      ) : (
+        <form onSubmit={handleSubmit} className="form">
+          <label>Nombre:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Ingresa tu nombre"
+          />
+          <label>Casa de Hogwarts:</label>
+          <input
+            type="text"
+            value={house}
+            onChange={(e) => setHouse(e.target.value)}
+            placeholder="Ingresa tu casa"
+          />
+          <button type="submit">Enviar</button>
+          {error && (
+            <p style={{ color: 'red' }}>Por favor chequea que la información sea correcta</p>
+          )}
+        </form>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
+
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,11 @@
+import "../Styles/Card.css";
+
+
 // eslint-disable-next-line react/prop-types
-const Card = ({message}) => {
+const Card = ({name,house}) => {
   return (
-    <div>
-       <h1> Saludos {message}</h1>
+    <div className="card">
+       <h3> {`Nos has indicado que tu nombre es ${name} y pertenences a la casa ${house}` }</h3>
     </div>
   
   )
